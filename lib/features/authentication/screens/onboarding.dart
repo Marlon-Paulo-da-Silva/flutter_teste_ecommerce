71bdba8e7de019a9a1e9ca991216ab22198299f6
Marlon Paulo <marlon.paulo.silva@outlook.com>
2024-05-28 14:02:34 -0300

    commit para as telas de splash, falta mudar os gifs

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -14,29 +14,47 @@ class OnBoardingScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
-            children: [
-              Column(
-                children: [
-                  Image(
-                    width: THelperFunctions.screenWidth() * 0.8,
-                    height: THelperFunctions.screenHeight() * 0.6,
-                    image: const AssetImage(TImages.onBoardingImage1)
-                    ),
-                    Text(
-                      TTexts.onBoardingSubTitle1,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                      textAlign: TextAlign.center
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwItems),
-                    Text(
-                      TTexts.onBoardingSubTitle1,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                      textAlign: TextAlign.center
-                      )
-                ],
-              )
+            children: const [
+              OnBoardingPage(image: TImages.onBoardingImage1, title: TTexts.onBoardingTitle1, subTitle: TTexts.onBoardingSubTitle1),
+              OnBoardingPage(image: TImages.onBoardingImage1, title: TTexts.onBoardingTitle1, subTitle: TTexts.onBoardingSubTitle1),
+              OnBoardingPage(image: TImages.onBoardingImage1, title: TTexts.onBoardingTitle1, subTitle: TTexts.onBoardingSubTitle1),
             ],
           )
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key, required this.image, required this.title, required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defaultSpace),
+      child: Column(
+        children: [
+          Image(
+            width: THelperFunctions.screenWidth() * 0.8,
+            height: THelperFunctions.screenHeight() * 0.6,
+            image: AssetImage(image)
+            ),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems),
+            Text(
+              subTitle,
+              style: Theme.of(context).textTheme.bodyMedium,
+              textAlign: TextAlign.center
+              )
         ],
       ),
     );
